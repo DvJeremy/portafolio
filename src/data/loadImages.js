eb0b6c5e7f430447717049a475714e4b0b67e326
dvjeremy <jeremybustamante42@gmail.com>
2025-10-24 16:54:24 -0500

    eliminar basura

--- src/data/loadImages.js
+++
@@ -1,5 +0,0 @@
-export function loadImages(folder) {
-  // Importa todas las imágenes de la carpeta usando glob
-  const modules = import.meta.glob(`../assets/${folder}/*.{png,jpg,jpeg,gif}`, { eager: true });
-  return Object.values(modules).map(m => m.default);
-}
